4c4a63d0eb7786c169c1880fb6a17bca6f5c8b38
Josiana Silva <josianasilva655@gmail.com>
2023-02-25 19:39:45 -0300

    feat: Adicionando as funções e atualizando o bot

---
+++ src/funcaoInfo.py
@@ -0,0 +1,116 @@
+import json
+import boto3
+import urllib.request
+
+
+def get_pokemon_info(name,atributo):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}/"
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(req)
+
+    
+    if response.status == 200:
+        data = json.loads(response.read().decode())
+        if atributo=='weight' or atributo == 'height':
+            info = data[str(atributo)]
+            return info
+        else:
+            attack = data["stats"][1]["base_stat"]
+            defense = data["stats"][2]["base_stat"]
+            hp = data["stats"][0]["base_stat"]
+            status = {"attack": attack, "defense": defense, "hp": hp}
+            return status[atributo]
+
+def prepareResponse(event, msgText):
+    response = {
+		  "sessionState": {
+		    "dialogAction": {
+		      "type": "Close"
+		    },
+		    "intent": {
+		      "name": event['sessionState']['intent']['name'],
+				  "state": "Fulfilled"
+		    }
+		  },
+		  "messages": [
+	       {
+	         "contentType": "PlainText",
+	         "content": msgText
+	        }
+	       ]
+	   }
+    
+    return response
+
+def check_pokemon_name(name):
+    
+    def levenshtein_distance(s, t):
+        m, n = len(s), len(t)
+        d = [[0] * (n + 1) for i in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            d[i][0] = i
+        for j in range(1, n + 1):
+            d[0][j] = j
+            
+        for j in range(1, n + 1):
+            for i in range(1, m + 1):
+                if s[i - 1] == t[j - 1]:
+                    d[i][j] = d[i - 1][j - 1]
+                else:
+                    substitution = d[i - 1][j - 1] + 1
+                    insertion = d[i][j - 1] + 1
+                    deletion = d[i - 1][j] + 1
+                    d[i][j] = min(substitution, insertion, deletion)
+                
+        return d[m][n]
+
+    pokemon_names = []
+
+    with open("pokemon_names.txt", "r") as arquivo:
+        file_content = arquivo.read()
+    
+    
+    pokemon_names = file_content.split('\n')
+    best_match = ""
+    min_distance = float("inf")
+    
+    for pokemon_name in pokemon_names:
+        distance = levenshtein_distance(name.lower(), pokemon_name.lower())
+        
+        if distance < min_distance:
+            min_distance = distance
+            best_match = pokemon_name
+    
+    if len(name)<3:
+        min_distance=10;
+    if min_distance > 3:
+        return None
+    
+    return best_match
+
+def translate_word(word):
+    translations = {
+        "peso": "weight",
+        "altura": "height",
+        "ataque": "attack", 
+        "defesa": "defense", 
+        "vida": "hp"
+    }
+    return translations.get(word.lower(), word)
+
+
+
+def lambda_handler(event, context):
+    pokemon=check_pokemon_name(event['pokemon'])
+    atributo=translate_word(event['atributo'])
+    if pokemon != None:
+        return {
+            'pokemon': pokemon,
+            'atributo': get_pokemon_info(pokemon,atributo)
+        }
+    else:
+        return {
+            'pokemon': 'error',
+            'atributo': 'error'
+        }
---
+++ src/funcaoPrincipal.py
@@ -0,0 +1,100 @@
+import json
+import boto3
+
+lambda_client = boto3.client('lambda')
+
+def prepareResponse(event, msgText):
+    response = {
+		  "sessionState": {
+		    "dialogAction": {
+		      "type": "Close"
+		    },
+		    "intent": {
+		      "name": event['sessionState']['intent']['name'],
+				  "state": "Fulfilled"
+		    }
+		  },
+		  "messages": [
+	       {
+	         "contentType": "PlainText",
+	         "content": msgText
+	        }
+	       ]
+	   }
+    
+    return response
+
+def responseCard(event, context):
+    # Faz alguma lógica com base na entrada do usuário
+    message = "sucesso"
+    # Cria uma resposta que será enviada de volta para o Amazon Lex V2
+    response = {
+          "sessionState": {
+            "dialogAction": {
+              "type": "Close"
+            },
+            "intent": {
+              "name": event['sessionState']['intent']['name'],
+                  "state": "Fulfilled"
+            }
+          },
+          "messages": [
+           {
+            "contentType": "ImageResponseCard",
+            "imageResponseCard": {
+                "title": "Quem é esse pokemon",
+                "imageUrl": "https://polly-spint6.s3.amazonaws.com/pikachu.jpg",
+                "buttons": [
+                {
+                    "text": "bubassauro",
+                    "value": "bubassauro"
+                },
+                {
+                    "text": "pikachu",
+                    "value": "pikachu"
+                }
+                ]
+            }
+            }
+           ]
+       }
+     
+    return response
+
+
+def lambda_handler(event, context):
+    intentName = event['sessionState']['intent']['name']
+    response = prepareResponse(event,"Desculpe, poderia reescrever sua frase")
+    if intentName=='play':
+        response = prepareResponse(event,"Função em desenvolvimento")
+        
+    elif intentName=='info':
+        if event['sessionState']['intent']['slots']['pokemon'] != None:
+            
+            pokemon = event['sessionState']['intent']['slots']['pokemon']['value']['originalValue']
+            atributo = event['sessionState']['intent']['slots']['atributos']['value']['interpretedValue']
+            
+            payload = {'pokemon': pokemon, 'atributo': str(atributo)}
+            
+            response = lambda_client.invoke(FunctionName='pokebot-dev-func-info-pokemon', Payload=json.dumps(payload))
+            result = json.loads(response['Payload'].read().decode())
+            
+            if result['pokemon'] != 'error':
+                attributes = {
+                    'peso': f"O pokemon {result['pokemon'].capitalize()} tem o peso de {float(result['atributo'])/10} quilos.",
+                    'altura': f"O pokemon {result['pokemon'].capitalize()} tem a altura de {float(result['atributo'])/10} metros.",
+                    'vida': f"O pokemon {result['pokemon'].capitalize()} tem {result['atributo']} pontos de vida.",
+                    'ataque': f"O pokemon {result['pokemon'].capitalize()} tem {result['atributo']} pontos de ataque.",
+                    'defesa': f"O pokemon {result['pokemon'].capitalize()} tem {result['atributo']} pontos de defesa."
+                }
+                if atributo in attributes:
+                    response = prepareResponse(event, attributes[atributo])
+                else:
+                    response = prepareResponse(event, "Atributo inválido. Tente novamente.")
+            else:
+                response = prepareResponse(event, "Esse pokemon não existe!")
+        else:
+             response = prepareResponse(event,"Desculpe, reescreva sua frase passando qual pokemon deseja saber o atributo: " + "Ex: qual o peso do Pikachu")
+    elif intentName == 'saudacao':
+        response = prepareResponse(event, "deu certo!!")
+    return response
